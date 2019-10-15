87338a335a1e394faa2f350628ce8535e79fc276
hugh zhao <hughzhao0806@163.com>
2019-10-15 15:48:51 +0800

    根据指定url获取图片，都是网络请求

--- app/src/main/java/com/hughzhao/drygirl/MainActivity.java
+++ app/src/main/java/com/hughzhao/drygirl/MainActivity.java
@@ -2,12 +2,64 @@ package com.hughzhao.drygirl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private ImageView imageView;
+    private Button btn;
+    private PictureLoader loader;
+    private int currentNo=0;
+    private List<String> urls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loader = new PictureLoader();
+        initData();
+        initUI();
+    }
+
+    /**
+     * 初始化url数据
+     */
+    public void initData(){
+        urls = new ArrayList<>();
+        urls.add("http://ww4.sinaimg.cn/large/610dc034jw1f6ipaai7wgj20dw0kugp4.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f6gcxc1t7vj20hs0hsgo1.jpg");
+        urls.add("http://ww4.sinaimg.cn/large/610dc034jw1f6f5ktcyk0j20u011hacg.jpg");
+        urls.add("http://ww1.sinaimg.cn/large/610dc034jw1f6e1f1qmg3j20u00u0djp.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f6aipo68yvj20qo0qoaee.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f69c9e22xjj20u011hjuu.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f689lmaf7qj20u00u00v7.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/c85e4a5cjw1f671i8gt1rj20vy0vydsz.jpg");
+        urls.add("http://ww2.sinaimg.cn/large/610dc034jw1f65f0oqodoj20qo0hntc9.jpg");
+        urls.add("http://ww2.sinaimg.cn/large/c85e4a5cgw1f62hzfvzwwj20hs0qogpo.jpg");
+    }
+
+    /**
+     * 初始化UI，设置按钮监听
+     */
+    public void initUI(){
+        imageView = findViewById(R.id.girl);
+        btn = findViewById(R.id.flash_btn);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.flash_btn:
+                if(currentNo>9)
+                    currentNo=0;
+                loader.load(imageView,urls.get(currentNo));
+                currentNo++;
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/hughzhao/drygirl/PictureLoader.java
@@ -0,0 +1,115 @@
+package com.hughzhao.drygirl;
+
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.ImageView;
+
+import com.hughzhao.drygirl.util.HttpUtil;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class PictureLoader {
+    private ImageView imgView;
+    private String imgUrl;
+    private byte[] picBytes;
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what){
+                case 1:
+                    picBytes = (byte[])msg.obj;
+                    if(picBytes!=null){
+                        Bitmap bitmap = BitmapFactory.decodeByteArray(picBytes,0,picBytes.length);
+                        imgView.setImageBitmap(bitmap);//设置ImageView
+                    }
+                    break;
+            }
+        }
+    };
+
+    /**
+     * 加载url，获取照片byte数组
+     * @param imgView
+     * @param imgUrl
+     */
+    public void load(ImageView imgView,String imgUrl){
+        this.imgView = imgView;
+        this.imgUrl = imgUrl;
+        Drawable drawable = imgView.getDrawable();
+        if(drawable!=null && drawable instanceof BitmapDrawable){
+            Bitmap bitmap = ((BitmapDrawable)drawable).getBitmap();
+            if(bitmap!=null && bitmap.isRecycled()){
+                bitmap.recycle();//减小图片对资源的消耗,这里目前理解不行，不到位
+            }
+        }
+        new Thread(runnable).start();
+    }
+
+    /**
+     * OkHttp请求
+     */
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            HttpUtil.sendOkHttpRequest(imgUrl, new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    Message msg = new Message();
+                    msg.what=1;
+                    msg.obj=response.body().bytes();
+                    handler.sendMessage(msg);
+                }
+            });
+        }
+    };
+    /**
+     * 传统http请求
+     */
+//    Runnable runnable = new Runnable() {
+//        @Override
+//        public void run() {
+//            try{
+//                URL url = new URL(imgUrl);
+//                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+//                con.setRequestMethod("GET");
+//                con.setConnectTimeout(10000);
+//                if(con.getResponseCode()==200){
+//                    InputStream in = con.getInputStream();
+//                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+//                    byte[] bytes = new byte[1024];
+//                    int length = -1;
+//                    while((length=in.read(bytes))!=-1){
+//                        out.write(bytes,0,length);
+//                    }
+//                    picBytes = out.toByteArray();
+//                    in.close();
+//                    out.close();
+//                    Message msg = new Message();
+//                    msg.what=1;
+//                    handler.sendMessage(msg);
+//
+//                }
+//            }catch (IOException e){
+//                e.printStackTrace();
+//            }
+//        }
+//    };
+}
